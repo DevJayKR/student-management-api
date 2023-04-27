c3a2f2c3c9db6ba0034dbcf12f9e38c43729f2c8
Junsung Choi <choiexe1@gmail.com>
2023-04-28 02:22:40 +0900

    feat: body parser, logger middleware

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 5000;
 const api = require("./src/api/router");
 
+app.use(logger);
+
 app.use("/api", api);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -13,3 +16,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
 });
+
+function logger(req, res, next) {
+  console.log(`[${req.method}] ${req.url}`);
+}
