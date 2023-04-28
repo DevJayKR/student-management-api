3cfade72d27c3d2d512db867d4b99982bcc236da
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-04-28 15:12:44 +0900

    feat : add school controller

--- src/api/school/school.controller.js
+++ src/api/school/school.controller.js
@@ -1,8 +1,31 @@
 const SchoolService = require("./school.service");
 const Service = new SchoolService();
 
-exports.findAllSchool = async () => await Service.findAllSchool();
-exports.findOneSchool = (id) => Service.findOneSchool(+id);
-exports.createSchool = async ({ schoolName, username, password }) => await Service.createSchool({ schoolName, username, password });
-exports.deleteSchool = (id) => Service.deleteSchool(+id);
-exports.updateSchool = (id) => Service.updateSchool(+id, {});
+// exports.findAllSchool = async () => await Service.findAllSchool();
+// exports.findOneSchool = (id) => Service.findOneSchool(+id);
+// exports.createSchool = async ({ schoolName, username, password }) => await Service.createSchool({ schoolName, username, password });
+// exports.deleteSchool = (id) => Service.deleteSchool(+id);
+// exports.updateSchool = (id) => Service.updateSchool(+id, {});
+
+module.exports = {
+
+    findAllSchool: async (req, res, next) => {
+        try {
+            const school = Service.findAllSchool();
+            if (school.length === 0) throw new Error("Not Found");
+            return res.status(200).send({ data: school, message: "Success to find" });
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    findOneSchool: async (req, res, next) => {
+        try {
+            const school = Service.findOneSchool(req);
+            if (school.length === 0) throw new Error("Not Found");
+            return res.status(200).send({ data: school, message: "Success to find" })
+        } catch (error) {
+            next(error);
+        }
+    }
+}
--- src/api/school/school.service.js
+++ src/api/school/school.service.js
@@ -1,50 +1,43 @@
 const { ulid } = require("ulid");
+const model = require('./school.model');
 
 module.exports = class SchoolService {
 	//TODO: DATABASE 연결해서 생성
 	schools = [];
 	id = 1;
 
-	async createSchool({ schoolName, username, password }) {
-		const school = {
-			id: this.id++,
-			schoolName,
-			username,
-			password,
-			authCode: ulid(),
-		};
-
-		this.schools.push(school);
-
-		return school;
+	async createSchool(school_id, school_name, school_email) {
+		const dto = [school_id, school_name, school_email];
+		return model.create(dto);
 	}
 
 	async findAllSchool() {
-		return this.schools;
+		return model.find();
 	}
 
-	findOneSchool(id) {
-		return this.schools.find((school) => school.id === id);
+	findOneSchool(school_id) {
+
+		return model.findbyId(school_id);
 	}
 
-	findSchoolIndex(id) {
-		return this.schools.findIndex((school) => school.id === id);
-	}
+	// findSchoolIndex(id) {
+	// 	return this.schools.findIndex((school) => school.id === id);
+	// }
 
-	deleteSchool(id) {
-		const index = this.findSchoolIndex(id);
+	deleteSchool(school_id) {
+		// const index = this.findSchoolIndex(id);
 
-		if (index === -1) {
-			return {
-				success: false,
-				message: "존재하지 않는 학교 아이디입니다.",
-			};
-		}
+		// if (index === -1) {
+		// 	return {
+		// 		success: false,
+		// 		message: "존재하지 않는 학교 아이디입니다.",
+		// 	};
+		// }
 
-		const school = this.findOneSchool(id);
+		// const school = this.findOneSchool(id);
 
-		this.schools.pop(index);
+		// this.schools.pop(index);
 
-		return school;
+		// return school;
 	}
 };
