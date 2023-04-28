e6690ea40338c550ba2d7dac2586ca196314996e
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-04-28 15:12:25 +0900

    feat : add mysql connect pool

---
+++ src/api/loaders/db.js
@@ -0,0 +1,6 @@
+const mysql = require('mysql2');
+const { host, user, password, database } = require('../config');
+const pool = mysql.createPool({
+    host, user, password, database, dateStrings: true
+});
+module.exports = pool.promise();
