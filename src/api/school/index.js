2a55bbdab8496315a9dfc1e912615909358d9a7d
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-04-29 00:58:52 +0900

    feat : Add school router

--- src/api/school/index.js
+++ src/api/school/index.js
@@ -1,31 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const controller = require('./school.controller');
 
-router.get("/", async (req, res) => res.send(await controller.findAllSchool()));
+//1 안
+// router.get("/", async (req, res) => res.send(await controller.findAllSchool()));
 
-router.get("/:id", (req, res) => {
-	const { id } = req.params;
-	res.send(controller.findOneSchool(id));
-});
+// router.get("/:id", (req, res) => {
+// 	const { id } = req.params;
+// 	res.send(controller.findOneSchool(id));
+// });
 
-router.post("/", async (req, res) => {
-	const { schoolName, username, password } = req.body;
+// router.post("/", async (req, res) => {
+// 	const { schoolName, username, password } = req.body;
 
-	const dto = {
-		schoolName,
-		username,
-		password,
-	};
+// 	const dto = {
+// 		schoolName,
+// 		username,
+// 		password,
+// 	};
 
-	const school = await controller.createSchool(dto);
+// 	const school = await controller.createSchool(dto);
 
-	res.send(school);
-});
+// 	res.send(school);
+// });
 
-router.delete("/:id", (req, res) => {
-	const { id } = req.params;
+// router.delete("/:id", (req, res) => {
+// 	const { id } = req.params;
 
-	res.send(controller.deleteSchool(id));
-});
+// 	res.send(controller.deleteSchool(id));
+// });
+
+//2안
+
+router.get('/all', controller.findAllSchool);
+router.get('/', controller.findOneSchool);
+router.post('/', controller.createSchool);
 
 module.exports = router;
